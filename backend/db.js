874693ae36d5600222c94810e1028513c1633449
OM TRIPATHI <tripathi111om@gmail.com>
2025-02-27 04:30:20 +0530

    "new changes"

--- backend/db.js
+++ backend/db.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-try {
-    await mongoose.connect('mongodb://localhost:27017')
-} catch (error) {
-    console.error("Something went wrong: "+ error.message)
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/finexa");
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("Something went wrong: " + error.message);
+    process.exit(1); // Exit process if connection fails
+  }
+};
 
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
-})
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
+  },
+  password: { type: String, required: true },
+});
 
-module.exports = mongoose.model('User',userSchema)
+const User = mongoose.model("User", userSchema);
+
+module.exports = { connectDb, User };
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 
+const app = express();
 
+const mainRouter = require("./routes/index");
+app.use("/api/v1", mainRouter);
+
+const {connectDb} = require("./db");
+
+connectDb()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("something went wrong " + err.message);
+  });
